318022037c6609cf9a45f2c9b215ab82d0e98694
saveliyB <saveliy199757@gmail.com>
2022-09-26 16:42:51 +0300

    add abstract class ExcelStateComponent for work store

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,33 +1,38 @@
-import {ExcelComponent} from '@core/ExcelComponent';
+import {createToolbar} from '@/components/toolbar/toolbar.temlate';
+import {$} from '@core/dom';
+import {ExcelStateComponent} from '@core/ExcelStateComponent';
 
-export class Toolbar extends ExcelComponent {
+export class Toolbar extends ExcelStateComponent {
     static className = 'excel__toolbar'
     constructor($root, options) {
         super($root, {
             name: 'Toolbar',
+            listeners: ['click'],
             ...options
         });
     }
+    prepare() {
+        const initState = {
+            textAlign: 'left',
+            fontWeight: 'normal',
+            textDecoration: 'none',
+            fontStyle: 'normal'
+        }
+        this.initState(initState)
+    }
+    get template() {
+        return createToolbar(this.state)
+    }
+
     toHTML() {
-        return `
-            <div class="button">
-                <span class="material-icons">format_align_left</span>
-            </div>
-            <div class="button">
-                <span class="material-icons">format_align_center</span>
-            </div>
-            <div class="button">
-                <span class="material-icons">format_align_right</span>
-            </div>
-            <div class="button">
-                <span class="material-icons">format_bold</span>
-            </div>
-            <div class="button">
-                <span class="material-icons">format_italic</span>
-            </div>
-            <div class="button">
-                <span class="material-icons">format_underline</span>
-            </div>
-        `
+        return this.template
+    }
+    onClick(event) {
+        const $target = $(event.target)
+        if ($target.data.type === 'button') {
+            const value = JSON.parse($target.data.value)
+            this.setState(value)
+            console.log(this.state)
+        }
     }
 }
