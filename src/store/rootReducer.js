8625eb2b4044811b738a3677ef02249a057e8e86
saveliyB <saveliy199757@gmail.com>
2022-10-19 16:18:06 +0300

    save style cell localstorage, bugfix

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -3,6 +3,7 @@ import {APPLY_STYLE, CHANGE_STYLES, CHANGE_TEXT, TABLE_RESIZE} from '@/store/typ
 export function rootReducer(state, action) {
     let prevState
     let field
+    let val
     switch (action.type) {
         case TABLE_RESIZE:
             field = action.data.type === 'col' ? 'colState' : 'rowState'
@@ -17,8 +18,14 @@ export function rootReducer(state, action) {
             return {...state, currentStyles: action.data}
         case APPLY_STYLE:
             field = 'stylesState'
+            val = state[field]
+            action.data.ids.forEach((id) => {
+                val[id] = {...val[id], ...action.data.value}
+            })
             return {
                 ...state,
+                [field]: val,
+                currentStyles: {...state.currentStyles, ...action.data.value}
             }
         default: return state
     }
